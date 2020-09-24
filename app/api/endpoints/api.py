d1d2f9130e055eeb0f546765286e1a1568263700
RobbeDeClercq <43599650+RobbeDeClercq@users.noreply.github.com>
2020-09-24 18:02:52 +0200

    Finished ex1 (not tested) + start ex2

--- app/api/endpoints/api.py
+++ app/api/endpoints/api.py
@@ -12,37 +12,64 @@ class IndexResponse(BaseModel):
 
 
 async def get_index(request: Request, year: int = 2004):
+    import datetime
+
     # EXERCISE 1
     # TODO INDEX_1
     # Make sure only 1996, 2004 and 2013 are valid
     # https://fastapi.tiangolo.com/tutorial/handling-errors/
     # tip: from fastapi import HTTPException
+    if year in {1996, 2004, 2013}:
+        # TODO INDEX_2
+        # Retrieve the latest index from external source for given base year
+        # https://statbel.fgov.be/en/themes/consumer-prices/health-index
+        index = 100.00
 
-    # TODO INDEX_2
-    # Retrieve the latest index from external source for given base year
-    # https://statbel.fgov.be/en/themes/consumer-prices/health-index
-    index = 100.00
 
-    # TODO INDEX_3
-    # Format and return the response
-    return {"index": index}
+        d = datetime.datetime.now()
+        m = int(d.strftime("%m")) - 1
+
+        url = "https://indexpub.economie.fgov.be/indexpub/api/ng-indsearch/get-consumer-health-index/{}/2020/IS".format(m)
+
+        data = Request.get(url).json()
+   
+        # TODO INDEX_3
+        # Format and return the response
+        for item in data:
+            if item["base"]["code"] == str(year):
+                index = item["value"]
+        
+        response.status_code = 200
+        return {"index": index}
+    else:
+        response.status_code = 400
+        return None
 
 
 async def get_zipcode_risk_factor(request: Request, zipcode: int):
+    import csv
     # EXERCISE 2
-    pass
     # TODO ZIPCODE_1
     # Validate if the zipcode entered has the correct format
     # (integer between 1000 and 9999)
+    if zipcode > 1000 and zipcode < 9999:
+        # TODO ZIPCODE_2
+        # Read the file app.data.zipcodes.csv and format data
+        with open('../../data/zipcodes.csv', newline='') as f:
+            reader = csv.reader(f)
+            data = list(reader)
 
-    # TODO ZIPCODE_2
-    # Read the file app.data.zipcodes.csv and format data
+        # TODO ZIPCODE_3
+        # Validate if the zipcode entered is in the dataset
+        for item in data:
+            if "-" in item[0]:
+                item[0].split(" - ")
+                if zipcode > item[0][0] and zipcode < item[0][1]:
+                    pass
 
-    # TODO ZIPCODE_3
-    # Validate if the zipcode entered is in the dataset
 
-    # TODO ZIPCODE_4
-    # Formulate appropriate response
+        # TODO ZIPCODE_4
+        # Formulate appropriate response
 
 
 async def get_zipcode_risk_factor_from_database(request: Request, zipcode: int):
